4806b0c13f8c367ed8ab8c28d747b6c6b3d28dba
rayoneka <rayoneka@gmail.com>
2020-03-20 10:30:51 +0300

    anorter update

--- first_bot.py
+++ first_bot.py
@@ -5,12 +5,21 @@ from random import randint
 
 bot = telebot.TeleBot("972288295:AAH666u2dez6FNSM6hhURbjVomoSTgKbBM8")
 
+last_message = None
+
 # если /help, /start
 @bot.message_handler(regexp='бот')
 def send_welcome(message):
     messages = ['а? кто здесь?', 'Ну да, я - бот! И что?', 'На самом деле я папа botFather. Только тсс!', 'Zzz...', 'Всем привет!', 'Ну я так не играю...']
     ln = len(messages) - 1
-    rnd = randint(0, ln)
+
+    global last_message
+    if last_message == None:
+        last_message = rnd = 0
+    else:
+        rnd = last_message
+        while rnd != last_message:
+            rnd = randint(0, ln)
 
     bot.send_message(message.chat.id, messages[rnd])
 
